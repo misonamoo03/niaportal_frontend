ac511d5365da0172c510f37dceeb9d2a8d08de59
misonamoo <bananacolor@gmail.com>
2021-12-30 10:13:27 +0900

    링크 변경

--- components/sports/pagination.vue
+++ components/sports/pagination.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="pagination">
-    <a style="cursor: pointer" class="first" v-if="first" @click="changeLink(1)"><i class="fa fa-angle-double-left" aria-hidden="true"></i></a>
-    <a style="cursor: pointer" class="prev" v-if="prev" @click="changeLink(startPageIndex - 1)"><i class="fa fa-angle-left" aria-hidden="true"></i></a>
+    <a href="javascript:void(0);" class="first" v-if="first" @click="changeLink(1)"><i class="fa fa-angle-double-left" aria-hidden="true"></i></a>
+    <a href="javascript:void(0);" class="prev" v-if="prev" @click="changeLink(startPageIndex - 1)"><i class="fa fa-angle-left" aria-hidden="true"></i></a>
       <span v-for="index in endPageIndex-startPageIndex + 1" :key="index">
-            <a style="cursor: pointer" :class="{'active': (startPageIndex + index - 1) == currentPage}"  @click="changePage(index)">{{ startPageIndex + index - 1 }}</a>
+            <a href="javascript:void(0);" :class="{'active': (startPageIndex + index - 1) == currentPage}"  @click="changePage(index)">{{ startPageIndex + index - 1 }}</a>
       </span>
-    <a style="cursor: pointer" class="next" v-if="next" @click="changeLink(endPageIndex + 1)"><i class="fa fa-angle-right" aria-hidden="true"></i></a>
-    <a style="cursor: pointer" class="end" v-if="end" @click="changeLink(pageCount)"><i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+    <a href="javascript:void(0);" class="next" v-if="next" @click="changeLink(endPageIndex + 1)"><i class="fa fa-angle-right" aria-hidden="true"></i></a>
+    <a href="javascript:void(0);" class="end" v-if="end" @click="changeLink(pageCount)"><i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
   </div>            
 </template>
 
